40cf0a3e10002c7339f68206c414099b38fd8e68
Sachin-Bisht <sachinbisht939@gmail.com>
2018-12-22 21:30:16 +0530

    base html added

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.urls import path
 from . import views
 
@@ -5,5 +6,8 @@ urlpatterns = [
 	path('', views.post_list, name='post_list'),
 	path('post/<int:pk>/', views.post_detail, name='post_detail'),
 	path('post/new/', views.post_new, name='post_new'),
-	path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+	path('post/<int:pk>	/edit/', views.post_edit, name='post_edit'),
+	url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+	url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
+	url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
 ]
